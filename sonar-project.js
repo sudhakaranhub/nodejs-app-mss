d2d055cf38642b74412bc0d01c6f5008d16ac2e5
sudhakaranhub <137423832+sudhakaranhub@users.noreply.github.com>
2023-08-02 12:31:31 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,1 +1,20 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://172.16.128.31:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'nodejs',
+	    'sonar.projectKey':'sonar.projectKey=nodejs',
+	    'sonar.login': 'sqa_fb49d6b67eeab77c96a478f087cc7d273e0d8937',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
 
